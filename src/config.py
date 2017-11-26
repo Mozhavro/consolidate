f69b59e27f3b12cc2a285cf51e364393084bc72b
mozhavro <mozhario@gmail.com>
2017-11-27 01:00:10 +0200

    fixed typo in config

--- src/config.py
+++ src/config.py
@@ -5,5 +5,5 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 NAME = "hanna"
 
-SCENES_DIR = os.path.join(BASE_DIR, "res/scenes/{name}".format(name=NAME))
+SCENES_DIR = os.path.join(BASE_DIR, "res/scene/{name}".format(name=NAME))
 DIALOG = os.path.join(BASE_DIR, "res/dialog/{name}.json".format(name=NAME))
