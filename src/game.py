d5c347ea5213df230637490576fc678d596c3c1b
mozhavro <mozhario@gmail.com>
2017-11-27 01:40:01 +0200

    removed hardcoded data, used model for initial setup

--- src/game.py
+++ src/game.py
@@ -6,46 +6,18 @@ from . import resources
 
 class Game:
     def start(self):
-        screen = """
-          ___ .~- ` `' "' ` -~. ____
-         :~+.-`  .-"-.  .-"~._  `-.+~:
-         !  /  -`     `       `'--~:.l
-          :'         .              '.
-         /
-        /-".        :  .            \`
-         .`      /.-"\ : `-  ^       :`
-         ^      "`    `.    \:'._  \ `!`
-        :     :-===-.    .-===-.\  .!/'.
-         '.; /     .             : :
-        : `.l   .mPm.\    .mPm.  |/    l
-       .     :                   :':   |
-           \ '        d:         ' /   :
-      :     '-:        "        :-;:   `
-      .       .     ._..._.     :::`   _
-     /        ::               ;::;.   !
-    .      .  :;:.           .::::;:   '
-    . :  : :  `:::'.       .'::::;::.   \
-    '/.  .  .  `::l '-. .-' '|:::::::.   ;
-    : |: :: ::__`.-.        _.-.::::::_  ;
-    .~"` \ \ :`"/    `-..- `     \"`//    "~.
-   /      \ \ .   ___ /\ ___      .//        \
-  :        \ \.-`  _.~l)=~  `' -.:"
-  `         '/  .    ":`-.       \            :
-        """
-        dialog ="Hello, world!"
-
         game_model = GameModel(config)
-        window = Window(game_model, screen)
+        window = Window(game_model)
         window.start()
         
 
 class GameModel:
     def __init__(self, config):
-        self._partner_name = config.NAME
+        self.partner_name = config.NAME
         self._dialog = resources.load_dialog()
 
-    def get_statement(id):
-        return self.dialog[id]
+    def get_statement(self, id):
+        return self._dialog[str(id)]
 
-    def get_scene(title):
+    def get_scene(self, title):
         return resources.get_scene(title)
--- src/window.py
+++ src/window.py
@@ -11,9 +11,8 @@ from asciimatics.exceptions import ResizeScreenError, StopApplication
 
 
 class Window:
-    def __init__(self, model=None, picture=None, additional_scenes=None):
+    def __init__(self, model, additional_scenes=None):
         self._additional_scenes = additional_scenes
-        self._picture = picture
         self._model = model
 
     def start(self):
@@ -26,7 +25,7 @@ class Window:
 
     def play_scenes(self, screen):
         scenes = [
-            Scene([GameFrame(screen, self._model, self._picture)], -1, name="Controls"),
+            Scene([GameFrame(screen, self._model)], -1, name="Controls"),
         ]
         if self._additional_scenes: scenes += self._additional_scenes
         
@@ -35,12 +34,13 @@ class Window:
 
 
 class GameFrame(Frame):
-    def __init__(self, screen, model, picture=''):
+    def __init__(self, screen, model):
         super(GameFrame, self).__init__(screen,
                                         screen.height,
                                         screen.width)
 
-        self._picture = picture
+        self._statement = model.get_statement(1)
+        self._picture = model.get_scene("start")
 
         self.palette = defaultdict(
             lambda: (Screen.COLOUR_WHITE, Screen.A_NORMAL, Screen.COLOUR_BLACK))
@@ -51,16 +51,32 @@ class GameFrame(Frame):
         layout = Layout([100], fill_frame=True)
         self.add_layout(layout)
 
+        answer_options = []
+        for key, answer in enumerate(self._statement["answers"], 1):
+            text = "{num}. {label}".format(num=key, label=answer["text"])
+            value = key
+            answer_options.append((text, value))
+
         self._answer_options = ListBox(
             Widget.FILL_FRAME,
-            [('1. wup', 1), ('2.dup', 2), ('2.dup', 3), ('2.dup', 4), ('2.dup', 5), ('2.dup', 6)],
+            answer_options,
             name="answer_options",
             on_change=self._on_pick)
 
         self._picture_display = TextBox(16, as_string=True)
         self._picture_display.disabled = True
 
+        self._remark = Text()
+        self._remark.value = self._statement['remark']
+        self._remark.disabled = True
+
+        self._statement_text = Text()
+        self._statement_text.value = self._statement['text']
+        self._statement_text.disabled = True
+
         layout.add_widget(self._picture_display)
+        layout.add_widget(self._remark)
+        layout.add_widget(self._statement_text)
         layout.add_widget(self._answer_options)
         layout.add_widget(
                 Label("Press `q` to quit."))
@@ -76,6 +92,8 @@ class GameFrame(Frame):
 
     def _update(self, frame_no):
         self._picture_display.value = self._picture
+        self._remark.value = self._statement['remark']
+        self._statement_text.value = self._statement['text']
 
         # Now redraw as normal
         super(GameFrame, self)._update(frame_no)
