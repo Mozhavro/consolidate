ee4e6a715d38e81df9e81973c29b8ffa279493dd
mozhavro <mozhario@gmail.com>
2017-11-27 01:00:49 +0200

    fixed scene resource helper method

--- src/resources.py
+++ src/resources.py
@@ -7,7 +7,7 @@ from . import config
 def get_scene(title):
     resource_file_path = os.path.join(config.SCENES_DIR, "{title}.txt".format(title=title))
     with open(resource_file_path) as scene_file:
-        return scene_file
+        return scene_file.read()
 
 
 def load_dialog():
