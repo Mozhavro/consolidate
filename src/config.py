f2776130d6d0759429a0bd295c53969c971f8a2e
mozhavro <mozhario@gmail.com>
2017-11-26 23:51:43 +0200

    fixed SCENES_DIR config setting

--- src/config.py
+++ src/config.py
@@ -5,5 +5,5 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 NAME = "hanna"
 
-SCENES_DIR = os.path.join(BASE_DIR, "res/scenes")
+SCENES_DIR = os.path.join(BASE_DIR, "res/scenes/{name}".format(name=NAME))
 DIALOG = os.path.join(BASE_DIR, "res/dialog/{name}.json".format(name=NAME))
