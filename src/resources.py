5777497a2b06d905af6acba19ba085e7df3664a4
mozhavro <mozhario@gmail.com>
2017-11-27 00:10:48 +0200

    fixed resources module

--- src/resources.py
+++ src/resources.py
@@ -7,8 +7,9 @@ from . import config
 def get_scene(title):
     resource_file_path = os.path.join(config.SCENES_DIR, "{title}.txt".format(title=title))
     with open(resource_file_path) as scene_file:
-        return scene_file.read()
+        return scene_file
 
 
 def load_dialog():
-    return json.load(config.DIALOG)
+    with open(config.DIALOG) as dialog_file:
+        return json.load(dialog_file)
