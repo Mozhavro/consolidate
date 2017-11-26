4ec91d23a8b67720e95d7f85535300b883c5c5ff
mozhavro <mozhario@gmail.com>
2017-11-26 23:36:08 +0200

    fixed dialog class

--- src/dialog.py
+++ src/dialog.py
@@ -1,9 +1,10 @@
 class Dialog:
-    def __init__(self, _current_statement=None, history=None):
-        self._current_statement = current_statement
+    def __init__(self, statement=None, answers=None,  history=None):
+        self._statement = statement or ''
+        self._answers = answers or list()
         self._history = history or list()
 
-    def add_item_to_history(self, actor=None, text):
+    def add_item_to_history(self, text, actor=None):
         if actor:
             item_string = "{actor}: {text}".format(actor=actor, text=text)
         else:
@@ -12,4 +13,4 @@ class Dialog:
         self._history.append(item_string)
 
     def get_history_as_text(self):
-        reurn "\n".join(self._history)
+        return "\n".join(self._history)
